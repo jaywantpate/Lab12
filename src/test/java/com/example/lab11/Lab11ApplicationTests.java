da8e372199fec823dfce4098c0df97e204fc25d5
jaywantvijaykumarpat <jaywantvijaykumarpat@cmail.carleton.ca>
2024-04-03 18:51:22 -0400

    Added Sanity Check and Unit Test

--- src/test/java/com/example/lab11/Lab11ApplicationTests.java
+++ src/test/java/com/example/lab11/Lab11ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.lab11;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Lab11ApplicationTests {
+
+   @Autowired
+    private NumberController controller;
+
+    @Autowired
+    private MockMvc mockmvc;
 
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void DoubletheNumberTest() throws Exception{
+        int number = 100;
+        this.mockmvc.perform(get("/double?number="+String.valueOf(number))).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(number*2))));
     }
 
 }
